a730176135c8f38ebc8429e38c911ab8a0c34a58
mickersicc <mickersicc@gmail.com>
2022-02-27 12:14:11 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Button, { Label } from '@smui/button';
+
+	function handleClick(event: CustomEvent) {
+		console.log('hello world');
+		debugger;
+	}
+</script>
+
+<h1>My Cat Sev</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button on:click={handleClick} variant="raised">
+	<Label>Click me</Label>
+</Button>
